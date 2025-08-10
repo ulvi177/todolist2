05652f15db7d257d7c7592bf3b5a7dd3a8dcd6e6
ulvi177 <ulvi-abdullayev@internet.ru>
2025-08-10 08:29:13 +0000

    todd

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 "use strict";
 $(document).ready(function () {
-
+    $('.btn').click(function (e) {  
+        e.preventDefault(); 
+        if (•••)  { 
+            let a = b => { 
+                for (let i = 1; i =>b; i++) { $('.minibox').append(`<h2>${b}</h2>`); } 
+            } 
+            let v = f => ••• ? ••• : $('.minibox').append(`<h2>${f}</h2>`);  
+            v($('input').val()); 
+        } 
+        ///// inputu təmizləmək ••• 
+    }) 
 });
