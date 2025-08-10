1d9a050a0cf5495e0c5a6af6c7417537bdcf5458
ulvi177 <ulvi-abdullayev@internet.ru>
2025-08-10 23:15:05 +0000

    toddo

--- app.js
+++ app.js
@@ -2,13 +2,16 @@
 $(document).ready(function () {
     $('.btn').click(function (e) {  
         e.preventDefault(); 
-        if (•••)  { 
+        if (!IsNaN($('.input').val)) {
+            !$.trim($("#inputme").val()) 
+            $('.input').val();        
             let a = b => { 
-                for (let i = 1; i =>b; i++) { $('.minibox').append(`<h2>${b}</h2>`); } 
+                for (let i = 1; i <= b; i++) { $('.minibox').append(`<h2>${b}</h2>`); } 
             } 
-            let v = f => ••• ? ••• : $('.minibox').append(`<h2>${f}</h2>`);  
+            let v = f =>  ? ••• : $('.minibox').append(`<h2>${f}</h2>`);  
             v($('input').val()); 
         } 
-        ///// inputu təmizləmək ••• 
+        ///// inputu təmizləmək 
+        $('input').val("");
     }) 
 });
